4f6bf023f9ebdd300818281d220036bc99da2e89
Mahmoud Abdulaal <mmh.00090@gmail.com>
2025-05-26 01:58:18 +0200

    ADC interrupt driver

---
+++ 17_ADC_Interrupt/Src/main.c
@@ -0,0 +1,43 @@
+#include "stm32f4xx.h"
+#include <stdint.h>
+#include <stdio.h>
+#include "uart.h"
+#include "adc.h"
+
+uint32_t sensor_value;
+
+static void adc_callback(void);
+
+int main(void)
+{
+	uart2_tx_init();
+	pa1_adc_interrupt_init();
+
+	start_conversion(); // no need to put this func inside
+						//while loop since continuous conversion
+						//is enabled
+
+	while(1)
+	{
+
+	}
+}
+
+
+static void adc_callback(void)
+{
+	sensor_value = ADC1->DR;
+	printf("sensor value :  %d\n\r" , (int)sensor_value);
+}
+
+void ADC_IRQHandler(void)
+{
+	/*check for eoc is SR*/
+	if((ADC1->SR & SR_EOC) != 0)
+	{
+		/*clear EOC */
+		ADC1->SR &= ~(SR_EOC);
+		adc_callback();
+	}
+}
+
